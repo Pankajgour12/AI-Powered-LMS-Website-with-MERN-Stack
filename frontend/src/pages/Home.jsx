ac41cfaf7bf5b458a1214a4bfdd20629e584c4d5
pankajgour12 <pankajgour5000@gmail.com>
2025-12-29 00:40:36 +0530

    home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,11 @@ const Home = () => {
     <div className='w-[100%] overflow-hidden'>
       <div className='w-[100%] lg:h-[140vh] h-[70vh] relative'>
         <Navbar/>
+        
+        <span className='lg:text-[70px] absolute md:text-[40%]
+        lg:top-[10%] top-[15%] w-[100%] flex items-center justify-center text-black font-bold text-[20px]' >Grow Skills to Advance</span>
+         <span className='lg:text-[70px] absolute md:text-[40%]
+        lg:top-[18%] top-[20%] w-[100%] flex items-center justify-center text-black font-bold text-[20px]'>Your Career Path</span>
       </div>
 
     </div>
