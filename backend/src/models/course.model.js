ec3e6c84dc7d7263a0c31d15bc556b8c54a2f3ed
pankajgour12 <pankajgour5000@gmail.com>
2025-12-30 21:24:34 +0530

    feat: add Course model with schema definition for course attributes

---
+++ backend/src/models/course.model.js
@@ -0,0 +1,79 @@
+import mongoose from "mongoose";
+
+
+
+const courseSchema = new mongoose.Schema({
+title:{
+    type:String,
+    required:true,
+},
+subTitle:{
+    type:String
+},
+
+description:{
+    type:String,
+    required:true,
+},
+
+category:{
+    type:String,
+    required:true,
+},
+level:{
+    type:String,
+    
+    enum:['Beginner','Intermediate','Advanced']
+},
+price:{
+    type:Number,
+    
+   
+},
+
+thumbnail:{
+    type:String,
+    required:true,
+},
+
+enrolledStudents:[
+    {type:mongoose.Schema.Types.ObjectId,
+ref:'User'
+}
+],
+
+lectures:[
+    {type:mongoose.Schema.Types.ObjectId,
+ref:'Lecture'
+}
+],
+
+creator:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+   
+},
+
+isPublished:{
+    type:Boolean,
+    default:false
+},
+
+reviews:[
+    {type:mongoose.Schema.Types.ObjectId,
+ref:'Review'
+}
+]
+
+
+
+
+
+},{timestamps:true})
+
+
+
+
+export const Course = mongoose.model('Course',courseSchema);
+
+export default Course;
