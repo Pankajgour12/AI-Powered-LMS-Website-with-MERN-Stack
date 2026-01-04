52812b216cb1b644469f0ff54bdd75d709cca897
pankajgour12 <pankajgour5000@gmail.com>
2026-01-04 20:04:25 +0530

    feat: implement createLecture and getCourseLecture functions; enhance editLecture for better error handling and video upload

--- backend/src/controllers/course.controller.js
+++ backend/src/controllers/course.controller.js
@@ -144,8 +144,81 @@ export const createLecture = async (req, res) =>{
     const lecture = await Lecture.create({lectureTitle})
     const course = await Course.findById(courseId)
 
+if(course){
+    course.lectures.push(lecture._id)
+    
+}
+ await course.populate('lectures')
+await course.save()
+    return res.status(201).json({message:"Lecture created successfully", lectur, course})
+
+
     } catch (error) {
-        res.status(500).json({ message: "Create Lecture error", error: error.message });
+        res.status(500).json({ message: "Failed to create lecture", error: error.message });
         
     }
 }
+
+
+
+
+export const getCourseLecture = async (req,res)=>{
+try {
+    const {courseId} = req.params
+    const course = await Course.findById(courseId)
+    if(!course){
+        return res.status(404).json({message:"Course not found"})
+    }
+    await course.populate('lectures')
+    await course.save()
+    return res.status(200).json({message:"Course lectures fetched successfully",course})
+    
+
+} catch (error) {
+    res.status(500).json({ message: "Failed to Get Course Lecture error", error: error.message });
+    
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+export const editLecture = async (req,res)=>{
+    try{
+        const {lectureId} = req.params
+        const {lectureTitle,  isPreviewFree} = req.body
+        let lecture = await Lecture.findById(lectureId)
+        if(!lecture){
+            return res.status(404).json({message:"Lecture are not found"})
+        } 
+
+        let videoUrl
+        if (req.file) {
+            videoUrl = await uploadOnCloudinary(req.file.path)
+            lecture.videoUrl = videoUrl
+        }
+        if(lectureTitle){
+            lecture.lectureTitle = lectureTitle
+        }
+        
+         lecture.isPreviewFree = isPreviewFree
+
+        await lecture.save()
+        return res.status(200).json({message:"Lecture updated successfully",lecture})   
+        
+        
+
+        
+        
+    }catch(error){
+        res.status(500).json({ message: "Edit Lecture error", error: error.message });       
+    }   
+}
+
