02ceb44381d65a112f93469b3418bf0e2e3b660b
pankajgour12 <pankajgour5000@gmail.com>
2026-01-08 12:50:31 +0530

    Done✅   ViewLectures page

--- frontend/src/pages/educator/lecture/ViewLectures.jsx
+++ frontend/src/pages/educator/lecture/ViewLectures.jsx
@@ -4,6 +4,7 @@ import {useSelector} from 'react-redux'
 import {useNavigate, useParams} from 'react-router-dom'
 import { serverUrl } from '../../../App'
 import { FaArrowLeftLong } from 'react-icons/fa6'
+import { FaPlayCircle } from 'react-icons/fa'
 
 
 
@@ -81,6 +82,60 @@ useEffect(()=>{
         </div>
 
       </div>
+
+      {/* right or bottom  */}
+     <div className='w-full md:w-1/3 bg-white rounded-2xl shadow-md p-6 border border-gray-200 h-fit  '>
+     <h2 className='text-xl font-bold mb-4 text-gray-800'>
+      All Lectures
+     </h2>
+     <div className='flex flex-col gap-3 mb-6'>
+    {selectedCourse?.lectures?.length > 0 ?
+    
+    (selectedCourse?.lectures?.map((lecture, index)=>(
+      <button key={index}
+      onClick={()=>setSelectedLecture(lecture)}
+      className={` flex items-center justify-between p-3 rounded-lg border transition text-left ${selectedLecture?._id === lecture._id 
+        ? 'bg-gray-200 border-gray-600'
+        :'hover: bg-gray-50 border-gray-300'
+      }`}
+      >
+        
+        
+       <h2 className='text-sm font-semibold text-gray-800'>{index+1}
+        {'. '} {lecture.lectureTitle}</h2>
+        <FaPlayCircle
+        className='text-lg text-green-400'/>
+
+      </button>
+    )))
+
+
+    : (<p className='text-gray-500'>No Lecture available</p>)
+    }
+
+
+     </div>
+     {/* Educator details  */}
+     <div className='mt-4 border-t pt-4'>
+ <h3 className='text-md font-semibold text-gray-700 mb-3'> Educator</h3>
+
+ <div className='flex items-center gap-4'>
+  <img src={creatorData?.photoUrl} alt="Educator"
+  className='w-16 h-16 rounded-full object-cover' />
+
+
+ </div>
+     </div>
+
+     <div>
+      <h2 className='text-lg font-medium text-gray-800 '>{creatorData.name}</h2>
+      <p className='text-sm text-gray-600'>{creatorData.description} </p>
+      <p className='text-sm text-gray-600'>{creatorData.email} </p>
+      
+     </div>
+
+     </div>
+
 
     </div>
   )
